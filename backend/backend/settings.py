8b33cdee93b7a242735343889c2ade9368be3721
juanl <juan.levickas@gmail.com>
2022-03-09 05:16:35 -0300

    hid django key, added requirements.txt file

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,10 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from .key import KEY
 from datetime import timedelta
 from pathlib import Path
 
-from secret_key import KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
--- backend/secret_key.py
+++
@@ -1,1 +0,0 @@
-KEY = 'django-insecure-=cnw^x6-7yv2jb2ua76m*f1m2+q!@c5(1=9m$2rf*&!a9j)2_%'
